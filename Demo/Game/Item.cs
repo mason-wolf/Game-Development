a4be026e26db76591bbafcce69804c6076803ec7
Mason Wolf <mason.wolf@arrayinfotech.com>
2020-07-19 16:41:50 -0500

    Improved Inventory System

--- Demo/Game/Item.cs
+++ Demo/Game/Item.cs
@@ -13,5 +13,10 @@ namespace Demo
         public Texture2D ItemTexture { get; set; }
         public int HealthAmount { get; set; }
         public Rectangle ItemRectangle { get; set; }
+        public int Column { get; set; }
+        public int Row { get; set; }
+        public int Index { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
--- Demo/interface/Inventory.cs
+++ Demo/interface/Inventory.cs
@@ -20,10 +20,15 @@ namespace Demo.Interface
         SpriteFont inventoryFont;
         KeyboardState oldKeyboardState;
         KeyboardState newKeyboardState;
-        int selectedItem = 0;
-        List<Item> itemList = Player.InventoryList;
+        public static int SelectedItem { get; set; }
+        public static List<Item> itemList = Player.InventoryList;
         public static bool InventoryOpen { get; set; }
         Vector2 Position { get; set; }
+
+        public enum Items
+        {
+            Chicken
+        }
 
         public Inventory(ContentManager content)
         {
@@ -44,7 +49,10 @@ namespace Demo.Interface
 
         public override void Update(GameTime gameTime)
         {
-            newKeyboardState = Keyboard.GetState();
+            if (InventoryOpen)
+            {
+                newKeyboardState = Keyboard.GetState();
+            }
 
             Position = new Vector2(StartArea.player.Position.X - 150, StartArea.player.Position.Y - 90);
             inventoryInterface = new Rectangle((int)Position.X, (int)Position.Y, 300, 200);
@@ -52,40 +60,46 @@ namespace Demo.Interface
             // Handle item selection in inventory menu.
             if (newKeyboardState.IsKeyDown(Keys.D) && oldKeyboardState.IsKeyUp(Keys.D))
             {
-                if (selectedItem != itemList.Count)
+                if (SelectedItem != itemList.Count)
                 {
-                    selectedItem++;
-
-                    // Ignore and skip the duplicate Rectangles from grid population.
-                    if (selectedItem == 7 || selectedItem == 16 || selectedItem == 25 || selectedItem == 34 || selectedItem == 43)
-                    {
-                        selectedItem += 2;
-                    }
+                    SelectedItem++;
                 }
             }
 
             if (newKeyboardState.IsKeyDown(Keys.A) && oldKeyboardState.IsKeyUp(Keys.A))
             {
-                if (selectedItem != 0)
+                if (SelectedItem != 0)
                 {
-                    selectedItem--;
-
-                    // Ignore and skip the duplicate Rectangles from grid population.
-                    if (selectedItem == 7 || selectedItem == 16 || selectedItem == 25 || selectedItem == 34 || selectedItem == 43)
-                    {
-                        selectedItem -= 2;
-                    }
+                    SelectedItem--;
                 }
             }
 
-            if (selectedItem == 42)
+            if (newKeyboardState.IsKeyDown(Keys.S) && oldKeyboardState.IsKeyUp(Keys.S))
             {
-                selectedItem = 0;
+                MoveToNextRow();
             }
 
+            if (newKeyboardState.IsKeyDown(Keys.W) && oldKeyboardState.IsKeyUp(Keys.W))
+            {
+                MoveToPreviousRow();
+            }
 
-            oldKeyboardState = newKeyboardState;
-            newKeyboardState = Keyboard.GetState();
+            // Reset selection if reached the end.
+            if (SelectedItem == 32)
+            {
+                SelectedItem = 0;
+            }
+
+            if (!InventoryOpen)
+            {
+                frames = 0;
+            }
+
+            if (InventoryOpen)
+            {
+                oldKeyboardState = newKeyboardState;
+                newKeyboardState = Keyboard.GetState();
+            }
         }
 
         public override void Draw(SpriteBatch spriteBatch)
@@ -93,10 +107,53 @@ namespace Demo.Interface
             if (InventoryOpen)
             {
                 spriteBatch.Draw(inventoryTexture, null, inventoryInterface);
-                spriteBatch.DrawString(inventoryFont, "Inventory", new Vector2(Position.X + 25, Position.Y + 10), Color.White, 0, new Vector2(0, 0), 1.5f, SpriteEffects.None, 0);
+                spriteBatch.DrawString(inventoryFont, "Items", new Vector2(Position.X + 25, Position.Y + 10), Color.White, 0, new Vector2(0, 0), 1.5f, SpriteEffects.None, 0);
                 DrawSelectedItem(spriteBatch);
             }
         }
+
+        /// <summary>
+        /// Adds an item to the player's inventory.
+        /// </summary>
+        /// <param name="item">Item Name</param>
+        public void AddToInventory(Items item)
+        {
+            switch (item)
+            {
+                case Items.Chicken:
+                    emptySlotFound = false;
+                    Item chicken = new Item();
+                    chicken.ItemTexture = Sprites.chickenTexture;
+                    int itemSlot = GetEmptySlot();
+                    itemList[itemSlot].ItemTexture = chicken.ItemTexture;
+                    itemList[itemSlot].Name = "Chicken";
+                    itemList[itemSlot].Description = "Restores health.";
+                    break;
+            }
+
+            emptySlotFound = true;
+        }
+
+        bool emptySlotFound = false;
+
+        public int GetEmptySlot()
+        {
+            int slot = 0;
+
+            if (!emptySlotFound)
+            {
+                foreach (Item item in itemList)
+                {
+                    if (item.ItemTexture == null)
+                    {
+                        slot = item.Index;
+                    }
+                }
+            }
+            return slot;
+        }
+        // Create a delay before drawing to allow time for positioning to update correctly.
+        int frames = 0;
 
         public void DrawSelectedItem(SpriteBatch spriteBatch)
         {
@@ -105,52 +162,116 @@ namespace Demo.Interface
 
             Rectangle itemSlot;
 
-            for (int i = 0; i < 5; ++i)
+            // Generate and populate grid items.
+            for (int i = 0; i < 4; ++i)
             {
                 for (int j = 0; j < 8; ++j)
                 {
+                    // Create a new rectangle with the coordinates for the item.
                     itemSlot = new Rectangle(x, y, 32, 32);
-                    Item item1 = new Item();
-                    item1.ItemRectangle = itemSlot;
-                    itemList.Add(item1);
+                    Item newItem = new Item();
+                    // Create a new item and assign the rectangle data.
+                    newItem.ItemRectangle = itemSlot;
+                    newItem.Column = i;
+                    newItem.Row = j;
+                    newItem.Name = "";
+                    newItem.Description = "";
+                    // Add the rectangle data to the inventory list.
+                    itemList.Add(newItem);
                     x += 31;
                 }
 
                 x = (int)Position.X + 25;
-                itemSlot = new Rectangle(x, y, 32, 32);
-                Item item2 = new Item();
-                item2.ItemRectangle = itemSlot;
-                itemList.Add(item2);
                 y += 31;
             }
 
-            Item chicken = new Item();
-            chicken.ItemTexture = Sprites.chickenTexture;
-            itemList[0].ItemTexture = chicken.ItemTexture;
-            itemList[1].ItemTexture = chicken.ItemTexture;
-
-            int itemPositionX = itemList[selectedItem].ItemRectangle.X;
-            int itemPositionY = itemList[selectedItem].ItemRectangle.Y;
-            int itemWidth = itemList[selectedItem].ItemRectangle.Width;
-            int itemHeight = itemList[selectedItem].ItemRectangle.Height;
-
+            // Assign each item an index.
             for (int i = 0; i < itemList.Count; i++)
             {
-                if (i < 45 && itemList[i].ItemTexture != null)
-                {
-                    spriteBatch.Draw(itemList[i].ItemTexture, new Rectangle(itemList[i].ItemRectangle.X, itemList[i].ItemRectangle.Y, 32, 32), Color.White);
-                }
+                itemList[i].Index = i;
             }
 
-            spriteBatch.Draw(selectedItemTexture, new Rectangle(itemPositionX, itemPositionY, 1, itemHeight + 1), Color.White);
-            spriteBatch.Draw(selectedItemTexture, new Rectangle(itemPositionX, itemPositionY, itemWidth + 1, 1), Color.White);
-            spriteBatch.Draw(selectedItemTexture, new Rectangle(itemPositionX + itemWidth, itemPositionY, 1, itemHeight + 1), Color.White);
-            spriteBatch.Draw(selectedItemTexture, new Rectangle(itemPositionX, itemPositionY + itemHeight, itemWidth + 1, 1), Color.White);
+            int itemPositionX = itemList[SelectedItem].ItemRectangle.X;
+            int itemPositionY = itemList[SelectedItem].ItemRectangle.Y;
+            int itemWidth = itemList[SelectedItem].ItemRectangle.Width;
+            int itemHeight = itemList[SelectedItem].ItemRectangle.Height;
 
+            AddToInventory(Items.Chicken);
+            AddToInventory(Items.Chicken);
+            AddToInventory(Items.Chicken);
 
-            if (itemList.Count > 100)
+            frames++;
+
+            if (frames > 10)
+            {
+                for (int i = 0; i < itemList.Count; i++)
+                {
+                    if (i < 45 && itemList[i].ItemTexture != null)
+                    {
+                        // Draw the item texture on the inventory slot.
+                        spriteBatch.Draw(itemList[i].ItemTexture, new Rectangle(itemList[i].ItemRectangle.X, itemList[i].ItemRectangle.Y, 32, 32), Color.White);
+
+                        // Draw the item name.
+                        spriteBatch.DrawString(inventoryFont, itemList[SelectedItem].Name, new Vector2(Position.X + 25, Position.Y + 175), Color.LightGreen, 0, new Vector2(0, 0), 1f, SpriteEffects.None, 0);
+                        spriteBatch.DrawString(inventoryFont, itemList[SelectedItem].Description, new Vector2(Position.X + 25, Position.Y + 185), Color.White, 0, new Vector2(0, 0), .7f, SpriteEffects.None, 0);
+                    }
+                }
+
+                spriteBatch.Draw(selectedItemTexture, new Rectangle(itemPositionX, itemPositionY, 1, itemHeight + 1), Color.White);
+                spriteBatch.Draw(selectedItemTexture, new Rectangle(itemPositionX, itemPositionY, itemWidth + 1, 1), Color.White);
+                spriteBatch.Draw(selectedItemTexture, new Rectangle(itemPositionX + itemWidth, itemPositionY, 1, itemHeight + 1), Color.White);
+                spriteBatch.Draw(selectedItemTexture, new Rectangle(itemPositionX, itemPositionY + itemHeight, itemWidth + 1, 1), Color.White);
+            }
+
+            if (itemList.Count > 200)
             {
                 itemList.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Move to next row on grid.
+        /// </summary>
+        public static void MoveToNextRow()
+        {
+            bool found = false;
+
+            foreach (Item item in itemList)
+            {
+                if (item.Column == itemList[SelectedItem].Column + 1
+                    && item.Row == itemList[SelectedItem].Row
+                    && found == false)
+                {
+                    found = true;
+
+                    if (item.Index <= itemList.Count)
+                    {
+                        SelectedItem = item.Index;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Move to previous row on grid.
+        /// </summary>
+        public static void MoveToPreviousRow()
+        {
+            bool found = false;
+
+            foreach (Item item in itemList)
+            {
+                if (item.Column == itemList[SelectedItem].Column - 1
+                     && item.Row == itemList[SelectedItem].Row
+                     && found == false)
+                {
+                    found = true;
+
+                    if (item.Index <= itemList.Count)
+                    {
+                        SelectedItem = item.Index;
+                    }
+                }
             }
         }
     }
